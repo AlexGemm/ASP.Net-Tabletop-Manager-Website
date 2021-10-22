3337edb1a3590b2c11e74ab7a4c095a83610ed5a
AlexG <agemmell@student.fitchburgstate.edu>
2021-10-22 13:32:43 -0400

    Added many new buttons to the statblock create page as well as made a controller and view for the encounter manager. Other than that, some small changes and clean up.

---
+++ Controllers/EncountersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnDWebsite.Controllers
+{
+    public class EncountersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,20 @@
 ﻿$(document).ready(function () {
 
+    $(".int-input").val(0);
+    $(".stats-input").val(0);
+
+    $("#infnsad").hide();
+    $("#infpd").hide();
+    $("#inflad").hide();
+    $("#infscd").hide();
+
     $("#hidensa").click(function () {
         $("#nsac").val("None");
         $("#nsarow").hide(500);
+    });
+
+    $("#infnsa").click(function () {
+        $("#infnsad").toggle(250);
     });
 
     $("#hidep").click(function () {
@@ -10,13 +22,36 @@
         $("#prow").hide(500);
     });
 
+    $("#infp").click(function () {
+        $("#infpd").toggle(250);
+    });
+
+    $("#hidela").click(function () {
+        $("#lac").val("None");
+        $("#larow").hide(500);
+    });
+
+    $("#infla").click(function () {
+        $("#inflad").toggle(250);
+    });
+
+    $("#hidesc").click(function () {
+        $("#scc").val("None");
+        $("#scrow").hide(500);
+    });
+
+    $("#infsc").click(function () {
+        $("#infscd").toggle(250);
+    });
+
     //Preset the create statblock page.
-    $("#bac").val("Attack Name: 0d0 Damagetype(Range 0ft / 0ft); Property, Property, Property \r\n- (Extra notes on the attack go here)");
+    $("#bac").val("Attack Name: 0d0 + 0 (+0 to hit)(Range 0ft / 0ft) Damage Type; Property, Property, Property \r\n- (Extra notes on the attack go here)");
     $("#nsac").val("Resources: (Resource Name) 0 \r\n - Ability Name (Charges/TimePeriod): Ability Details (Duration)");
     $("#pc").val("- Effect Name: Effect details.");
     $("#lac").val("Legendary Resistances: (Charges/Day) \r\nLegendary Actions Charges: (0)\r\n- Legendary Action Name (Cost): Details");
-    $("#scc").val("Spell Slots: 1 (0), 2 (0), 3 (0), 4 (0), 5 (0), 6 (0), 7 (∅), 8 (0), 9 (0)\r\n- Spell Name (spell level)(action type)(range): Spell description.");
+    $("#scc").val("Spell Slots: 1 (0), 2 (0), 3 (0), 4 (0), 5 (0), 6 (0), 7 (0), 8 (0), 9 (0)\r\n- Spell Name (spell level)(action type)(range): Spell description.");
     $("#enc").val("- ");
+    $("#sc").val("0 Passive Perception");
 
     //Add linebreaks to the index page.
     $("#bai").html($("#bai").html().replaceAll('\r\n', "<br>"));
